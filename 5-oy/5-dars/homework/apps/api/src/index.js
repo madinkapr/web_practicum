ac8fed529c83d70eae7e8b851dc7e2eb5855ac9e
madinkapr <madinabonu.primova@gmail.com>
2026-03-21 20:51:19 +0500

    refactoring classwork

---
+++ 5-oy/5-dars/homework/apps/api/src/index.js
@@ -0,0 +1,23 @@
+import { createServer } from "node:http"
+import { Server } from "socket.io"
+
+const PORT = 3000
+
+const httpServer = createServer( ( req, res ) => {
+
+	res.writeHead( 200, { "Content-Type": "text/plain" } )
+	res.end( "Socket server is running" )
+} )
+
+const io = new Server( httpServer, {
+	cors: {
+		origin: "*",
+		methods: [ "GET", "POST" ],
+	}
+} )
+
+
+httpServer.listen( PORT, "0.0.0.0", () => {
+
+	console.log( `Server listening on port ${ PORT }` )
+} )
