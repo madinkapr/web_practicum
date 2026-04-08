2edb49808d28e5a569373e308ea9f5894e35862d
madinkapr <madinabonu.primova@gmail.com>
2026-04-08 23:37:14 +0500

    fixed code

--- 5-oy/9-dars/classwork/pg-fastify-vanilla_2/apps/frontend/src/main.js
+++ 5-oy/9-dars/classwork/pg-fastify-vanilla_2/apps/frontend/src/main.js
@@ -8,10 +8,10 @@ const state = {
 	name: ""
 }
 
-const categories = document.getElementById( "categories" )
-const count = document.getElementById( "count" )
-const loadMore = document.getElementById( "loadMore" )
-const booksList = document.querySelector( ".books ul" )
+const categories = document.getElementById("categories")
+const count = document.getElementById("count")
+const loadMore = document.getElementById("loadMore")
+const booksList = document.querySelector(".books ul")
 const search = document.getElementById("search")
 
 await loadCategories()
@@ -29,7 +29,7 @@ categories.onchange = async () => {
 count.onchange = async () => {
 
 	state.page = 1
-	state.count = parseInt( count.value )
+	state.count = parseInt(count.value)
 	booksList.innerHTML = ""
 
 	await updateBooks()
@@ -42,7 +42,7 @@ loadMore.onclick = async () => {
 	await updateBooks()
 }
 
-search.oninput = async()=>{
+search.oninput = async () => {
 	state.page = 1
 	state.name = search.value
 	booksList.innerHTML = ""
@@ -52,50 +52,52 @@ search.oninput = async()=>{
 
 async function updateBooks() {
 
-	const books = await getBooks( state )
+	const books = await getBooks(state)
+
+	const regex = state.name ? new RegExp(`(${state.name})`, 'gi') : null
 
 	// console.log( books )
 
-	books.forEach( book => {
+	books.forEach(book => {
 
-		const li = document.createElement( "li" )
+		const li = document.createElement("li")
 		li.innerHTML = `
-			<img src="${ book.cover_url }" alt="${ book.name }" />
-			<h3>${ book.name }</h3>
-			<p>Author: ${ book.author }</p>
-			<p>Price: ${ book.price }</p>
-			<p>Category: ${ book.category_name }</p>
+			<img src="${book.cover_url}" alt="${book.name}" />
+			<h3>${regex ? book.name.replace(regex, '<b>$1</b>') : book.name}</h3>
+			<p>Author: ${regex ? book.author.replace(regex, '<b>$1</b>') : book.author}</p>
+			<p>Price: ${book.price}</p>
+			<p>Category: ${book.category_name}</p>
 		`
-		booksList.appendChild( li )
-	} )
+		booksList.appendChild(li)
+	})
 }
 
 async function loadCategories() {
 
-	const data = await fetch( `${ CONFIG.API_URL }/categories` )
+	const data = await fetch(`${CONFIG.API_URL}/categories`)
 	const categoriesList = await data.json()
 
-	categoriesList.forEach( category => {
+	categoriesList.forEach(category => {
 
-		const option = document.createElement( "option" )
+		const option = document.createElement("option")
 		option.value = category.id
 		option.textContent = category.name
-		categories.appendChild( option )
-	} )
+		categories.appendChild(option)
+	})
 }
 
-async function getBooks( { category, page, count, name } = {} ) {
+async function getBooks({ category, page, count, name } = {}) {
 
-	const query = new URLSearchParams( {
+	const query = new URLSearchParams({
 		category,
 		page,
 		count,
 		name
-	} )
+	})
 
-	const booksURL = `${ CONFIG.API_URL }/books?${ query.toString() }`
+	const booksURL = `${CONFIG.API_URL}/books?${query.toString()}`
 
-	const response = await fetch( booksURL )
+	const response = await fetch(booksURL)
 	const json = await response.json()
 
 	return json
