bd15ea623bdf2a774ce85b4ac63d4638c4a8dc73
madinkapr <madinabonu.primova@gmail.com>
2026-05-07 09:05:58 +0500

    initial commit

---
+++ 6-oy/6-dars/classwork/react/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export function Footer({color}){
+    return (
+        <footer style={{backgroundColor: color}}>
+            <h1>Footer</h1>
+        </footer>
+    )
+}
---
+++ 6-oy/6-dars/classwork/react/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export function Header({name}){
+    return (
+        <header>
+            <h1>Hello {name}</h1>
+            <p>Hello</p>
+        </header>
+    )
+}
